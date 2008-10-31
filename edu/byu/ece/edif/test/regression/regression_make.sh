2b66900fad80002fdf63e9fe76f7bf48af1dd05d
jamescarroll <jamescarroll@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2008-10-31 17:51:05 +0000

    WIP (will not "just work")
    Making the script easier for the user by moving variable assignments around, adding comments.

--- edu/byu/ece/edif/test/regression/regression_make.sh
+++ edu/byu/ece/edif/test/regression/regression_make.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #####################################################################
-# Regression test bash script
+# Regression test bash script using makefiles
 #
-# This script is used to test for non-deterministic behavior in the
-# BYU EDIF Tools JEdif toolchain.
+# This script is used to test for regression testing of the
+# BYU EDIF Tools, especially the JEdif toolchain.
 #
 # This script runs each of the test designs 
 # through each of the tools in the JEdif Toolchain (JEdifBuild,
@@ -15,43 +15,90 @@
 # along with an "options.mk" file that specifies the options to send
 # to the JEdif tools
 #
-# This script assumes that golden copies of each of these files has
+# This script has the ability to create golden copies, 
 # previously been created. Thus, the two copies of each design can
 # be compared (using the `diff' utility) for any differences.  Note
 # that the "timeStamp" is removed from all .edf files.
 #
 # Outline:
-#   1. Set up all the variables and options
+#   0. Set up all the variables and options (REQUIRED)
+#      A. Classpath
+#      B. Build type 
+#   1. Get a list of the files to test for the source directory
 #   2. Checkout SVN source and build a new JAR to test against
-#   3. For each design:
-#      A. Run each of the tools in the tool chain and output to test directory
-#   4. Strip the timeStamp from all .edf files
-#   5. Print results to screen and to log files
-#   6. Report success or failure (send e-mail on failure)
+#   3. Build with java, check for errors
+#   4. For each design:
+#      A. Run each of the tools in the tool chain 
+#      B. Output to test directory
+#   5. Strip the timeStamp from all .edf files
+#   6. Print results to screen and to log files
+#   7. Report success or failure (send e-mail on failure)
 # 
-# Authors: James Carroll <jcarroll@byu.net>
+# Authors: James Carroll <jcarroll@byu.net>, Derrick Gibelyou
 # $Id$
 # 
 #####################################################################
 
+# Enables running the script from other folders
+script_dir=`pwd`
+
+#####################################################################
+# You MUST set the values below.  Defaults will NOT "just work".
+#####################################################################
+
+#
+# Classpath
+#
+
+# Chose jar or working copy (golden or repository). Examples:
+#jar="${HOME}/jars/byuediftools-0.4.0.jar "
+#jar="/fpga2/jars/byuediftools-0.4.0.jar "
+jar=
+cp_golden=
+
+#workspace="${HOME}/workspace/sf_edif/:/fpga2/jars/JSAP-2.1.jar "
+workspace=
+cp_local
+
+#repository="${script_dir}/trunk:/fpga2/jars/JSAP-2.1.jar "
+repository=
+cp_repository
+
+# Dependencies
+# JSAP: Java Simple Argument Parser (martiansoftware.com/jsap/)
+#jsap_jar="/fpga2/jars/JSAP-latest.jar "
+jsap_jar=
+
+# JHDL (jhdl.org)
+#jhdl_jar="/fpga2/jars/JHDL.jar "
+jhdl_jar=
+
+#
+# What type of build do you want?
+#
+
+# golden: create the golden files in the golden directory from the jar file
+# local: create test files from local code
+# repository: create test files from the repository
+build="golden"
+#build="local"
+#build="repository"
+
+#####################################################################
+# Only change the following if you know what you are doing.
+#####################################################################
 
 user=`whoami`
 email="${user}@localhost"
-
 
 test_dir="./test"
 golden_dir="./golden"
 source_dir="./source"
 rep_dir="./trunk"
-script_dir=`pwd`
 
 globals=" GLOBALS=${script_dir}/global_conf.mk "
 makefile=" MAKEFILE=${script_dir}/makefile "
 make_cmd=" make -f options.mk $globals $makefile ";
-
-build="golden"
-#build="local"
-#build="repository"
 
 #
 # 1. Get a list of the files to test for the source directory
@@ -60,12 +107,6 @@ wd=`pwd`;
 cd $source_dir
 folders=`ls`;
 cd $wd;
-
-
-# Chose jar or working copy (golden or repository)
-jar="${HOME}/jars/byuediftools-0.4.0.jar "
-workspace="${HOME}/workspace/sf_edif/:/fpga2/jars/JSAP-2.1.jar "
-repository="${script_dir}/trunk:/fpga2/jars/JSAP-2.1.jar "
 
 echo
 if [ $build = "golden" ]; then
@@ -81,7 +122,7 @@ elif [ $build = "repository" ]; then
 	classpath=$repository
 	build_dir=$test_dir
 else
-	echo "You didn't specify the correct build: golden, local, or repository"
+	echo "You didn't specify the correct build.\n Please set variable 'build' to one of {golden, local, or repository}."
 	exit 1;
 fi
 echo
