cc579dac93c47f92e8c289839c539cf726d4a5db
jamescarroll <jamescarroll@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2008-11-04 19:39:13 +0000

    Added comments, including header comments

--- edu/byu/ece/edif/test/regression/global_conf.mk
+++ edu/byu/ece/edif/test/regression/global_conf.mk
@@ -1,6 +1,25 @@
-JAVA=java
-HOME=/fpga1/users/dsgib/
-CLASSPATH=-cp $(HOME)/workspace/sf_edif/:$(HOME)/jars/JSAP.jar
-JAVA_OPTS=-Xmx512M
+#####################################################################
+# Global Configuration file to accompany makefile regression test.
+#
+# User *must* edit this file to set environment variables
+#
+# Author: Derrick Gibelyou, James Carroll <jcarroll@byu.net>
+#
+# $Id$
+#####################################################################
 
+# Java runtime environment command (usually just `java') 
+JAVA=java
+
+# Your home folder (used for _______)
+HOME="/fpga1/users/dsgib/ "
+
+# byuediftools path, jar files
+CLASSPATH="-cp $(HOME)/workspace/sf_edif/:$(HOME)/jars/JSAP.jar "
+
+# Parameter options for JRE 
+JAVA_OPTS="-Xmx512M "
+
+# Modelsim executable director (usually ends with `/bin/')
+MODELSIM="/fpga2/modeltech/bin/ "
 
