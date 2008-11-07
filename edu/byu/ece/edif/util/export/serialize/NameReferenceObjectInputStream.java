71a9a3018a707fb870d7a3a792d59f9e76831226
jamescarroll <jamescarroll@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2008-11-07 22:06:29 +0000

    added license to header comment

--- edu/byu/ece/edif/util/export/serialize/NameReferenceObjectInputStream.java
+++ edu/byu/ece/edif/util/export/serialize/NameReferenceObjectInputStream.java
@@ -1,3 +1,25 @@
+/* 
+ * Maps deserialized EDIF name reference objects to real EDIF objects 
+ *
+ * Copyright (c) 2008 Brigham Young University
+ * 
+ * This file is part of the BYU EDIF Tools.
+ * 
+ * BYU EDIF Tools is free software: you may redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ * 
+ * BYU EDIF Tools is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * A copy of the GNU General Public License is included with the BYU EDIF Tools.
+ * It can be found at /edu/byu/edif/doc/gpl2.txt. You may also get a copy of the
+ * license at <http://www.gnu.org/licenses/>.
+ * 
+ */
 package edu.byu.ece.edif.util.export.serialize;
 
 import java.io.IOException;
@@ -7,7 +29,7 @@ import java.io.ObjectInputStream;
 import edu.byu.ece.edif.core.EdifEnvironment;
 
 /**
- * This is a customized version of ObjectInputStream used for EDIF name
+ * This is a customized version of ObjectInputStream used for mapping EDIF name
  * reference objects to the real EDIF objects from the given
  * EdifEnvironment during serialization. The class is intended to be used
  * in conjunction with NameReferenceObjectOutputStream.
