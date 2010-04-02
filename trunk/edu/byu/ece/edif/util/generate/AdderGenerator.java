a1212d8977f56f41109a17e601923e8ad4c72443
brianpratt <brianpratt@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2010-04-02 16:04:25 +0000

    Added an extra command-line option to the module generators for the user to specify the weight of the LSB of the arithmetic.

--- trunk/edu/byu/ece/edif/util/generate/AdderGenerator.java
+++ trunk/edu/byu/ece/edif/util/generate/AdderGenerator.java
@@ -5,17 +5,27 @@ import edu.byu.ece.edif.core.EdifPort;
 
 
 public class AdderGenerator {
+	
 	public static void main(String[] args) throws IOException {
+
+		if (args.length < 2 || args.length > 3) {
+			System.out.println("USAGE: java AdderGenerator <Module Name>"
+					+ " <Input Signal Width> [Weight of LSB (default=0)]");
+			System.exit(-1);
+		}
 		
 	    String name = args[0];
 	    int length = Integer.parseInt(args[1]);
+        int weight = 0;
+		if (args.length == 3)
+			weight = Integer.parseInt(args[2]);
 	    
 		CircuitGenerator generator = new CircuitGenerator(name);
 		generator.addPort("a", length, EdifPort.IN);
 		generator.addPort("b", length, EdifPort.IN);
 		generator.addPort("c", length+1, EdifPort.OUT);
 		
-		Adder adder0 = new Adder(name+"0", length, generator, 0);
+		Adder adder0 = new Adder(name + "0", length, generator, weight);
 		
 		
 		for (int i = 0; i < length; i++) {
--- trunk/edu/byu/ece/edif/util/generate/MultiplierGenerator.java
+++ trunk/edu/byu/ece/edif/util/generate/MultiplierGenerator.java
@@ -6,17 +6,27 @@ import edu.byu.ece.edif.core.EdifPort;
 
 public class MultiplierGenerator {
 
-public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+
+		if (args.length < 2 || args.length > 3) {
+			System.out.println("USAGE: java MultiplierGenerator <Module Name>"
+					+ " <Input Signal Width> [Weight of LSB (default=0)]");
+			System.exit(-1);
+		}
 	
         String name = args[0];
         int length = Integer.parseInt(args[1]);
+        int weight = 0;
+		if (args.length == 3)
+			weight = Integer.parseInt(args[2]);
     
 		CircuitGenerator generator = new CircuitGenerator(name);
 		generator.addPort("a", length, EdifPort.IN);
 		generator.addPort("b", length, EdifPort.IN);
 		generator.addPort("c", 2*length, EdifPort.OUT);
 		
-		UnsignedMultiplier mult0 = new UnsignedMultiplier(name+"0", length, generator, 0);		
+		UnsignedMultiplier mult0 = new UnsignedMultiplier(name + "0", length,
+				generator, weight);		
 		
 		for (int i = 0; i < length; i++) {
 			generator.connect(generator.getPort("a", i), mult0.getPort("X", i));
