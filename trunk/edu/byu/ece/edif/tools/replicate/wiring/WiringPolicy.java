75ac37919058f3c466f743c08fe0f7c9d8620872
ee320ta <ee320ta@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2010-04-16 16:05:15 +0000

    Comments

--- trunk/edu/byu/ece/edif/tools/replicate/wiring/WiringPolicy.java
+++ trunk/edu/byu/ece/edif/tools/replicate/wiring/WiringPolicy.java
@@ -2,6 +2,11 @@ package edu.byu.ece.edif.tools.replicate.wiring;
 
 import java.util.List;
 
+/**
+ * An interface to specify the wiring policy between sources/sinks when the replication type differs.
+ * 
+ * The only WiringPolicy currently available is the ModuloIterationWiringPolicy.
+ */
 public interface WiringPolicy {
     
     public void connectSourcesToSinks(List<PortConnection> sources, List<? extends PortConnection> sinks, NetManager netManager);
