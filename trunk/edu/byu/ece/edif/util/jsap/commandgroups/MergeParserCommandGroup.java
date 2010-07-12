ef5ab3e894be894cbc34c2808d1880e0fb3c6a61
ee320ta <ee320ta@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2010-07-12 22:22:18 +0000

    Created Class objects for reusable commands and added static versions of these objects so a subset of the commands can be used.

--- trunk/edu/byu/ece/edif/util/jsap/commandgroups/MergeParserCommandGroup.java
+++ trunk/edu/byu/ece/edif/util/jsap/commandgroups/MergeParserCommandGroup.java
@@ -39,44 +39,24 @@ import edu.byu.ece.edif.tools.LogFile;
 import edu.byu.ece.edif.util.merge.EdifMergeParser;
 import edu.byu.ece.edif.util.parse.ParseException;
 
-/**
- * TODO:
- * <ul>
- * <li> For getEdifEnvironment, how do I make sure that the parser has the
- * options in this group?
- * <li> Allow for different primitive libraries
- * </ul>
- */
 public class MergeParserCommandGroup extends JEdifParserCommandGroup {
 
-    public MergeParserCommandGroup() {
+	public static final MultipleDirectoryFlaggedOption DIRECTORY_FLAG = new MultipleDirectoryFlaggedOption();
+
+	public static final MultipleIncludeFileFlaggedOption INCLUDE_FLAG = new MultipleIncludeFileFlaggedOption();
+	
+	public static final BlackBoxSwitch BLACK_BOX_SWITCH = new BlackBoxSwitch();
+	
+	public MergeParserCommandGroup() {
         super();
 
         // Directory flag
-        FlaggedOption directory_list = new FlaggedOption(DIR_FLAG);
-        directory_list.setStringParser(JSAP.STRING_PARSER);
-        directory_list.setRequired(JSAP.NOT_REQUIRED);
-        directory_list.setShortFlag('d');
-        directory_list.setLongFlag(DIR_FLAG);
-        directory_list.setAllowMultipleDeclarations(JSAP.MULTIPLEDECLARATIONS);
-        directory_list.setList(JSAP.LIST);
-        directory_list.setListSeparator(',');
-        directory_list.setHelp("Comma-separated list of directories containing external EDIF files to be included.");
-        this.addCommand(directory_list);
-
+        this.addCommand(DIRECTORY_FLAG);
         // Forced file inclusion flag
-        FlaggedOption file_option = new FlaggedOption(INCLUDE_FILE_FLAG);
-        file_option.setStringParser(JSAP.STRING_PARSER);
-        file_option.setRequired(JSAP.NOT_REQUIRED);
-        file_option.setShortFlag('f');
-        file_option.setLongFlag(INCLUDE_FILE_FLAG);
-        file_option.setAllowMultipleDeclarations(JSAP.MULTIPLEDECLARATIONS);
-        file_option.setList(JSAP.LIST);
-        file_option.setListSeparator(',');
-        file_option
-                .setHelp("Similar to the previous option, but each external EDIF file is named explicitly--including the path to the file.");
-        this.addCommand(file_option);
-
+        this.addCommand(INCLUDE_FLAG);
+        // Black boxes
+        this.addCommand(BLACK_BOX_SWITCH);
+        
         // Flatten Flag
         Switch no_flatten_option = new Switch(NO_FLATTEN_FLAG);
         no_flatten_option.setShortFlag(JSAP.NO_SHORTFLAG);
@@ -92,17 +72,7 @@ public class MergeParserCommandGroup extends JEdifParserCommandGroup {
         no_open_pins_option.setHelp("Do not allow parser to infer open pins");
         this.addCommand(no_open_pins_option);
 
-        Switch blackboxes_ok_option = new Switch(BLACK_BOX_OK_SWITCH);
-        blackboxes_ok_option.setShortFlag(JSAP.NO_SHORTFLAG);
-        blackboxes_ok_option.setLongFlag(BLACK_BOX_OK_SWITCH);
-        blackboxes_ok_option.setDefault("false");
-        blackboxes_ok_option.setHelp("Allow parser to continue if blackbox definitions are not found");
-        this.addCommand(blackboxes_ok_option);
 
-    }
-
-    public static Collection<String> getDirectoryCollection(JSAPResult result) {
-        return Arrays.asList(result.getStringArray(DIR_FLAG));
     }
 
     public static EdifEnvironment getEdifEnvironment(JSAPResult result) {
@@ -115,15 +85,15 @@ public class MergeParserCommandGroup extends JEdifParserCommandGroup {
         }
         //parse the edif files if nessisary
         else {
-            boolean quitOnError = !result.getBoolean(BLACK_BOX_OK_SWITCH);
+            boolean quitOnError = !result.getBoolean(BlackBoxSwitch.BLACK_BOX_OK_SWITCH);
             boolean allow_open_pins = !result.getBoolean(NO_OPEN_PINS_SWITCH);
 
             LogFile.out().println("Parsing");
             try {
                 Set<String> searchDirs = EdifMergeParser.createDefaultDirs();
-                searchDirs.addAll(getDirectoryCollection(result));
+                searchDirs.addAll(MultipleDirectoryFlaggedOption.getDirectoryCollection(result));
                 top = EdifMergeParser.parseAndMergeEdif(getInputFileName(result), searchDirs,
-                        getIncludeFileCollection(result), primitiveLibrary, allow_open_pins, quitOnError);
+                		MultipleIncludeFileFlaggedOption.getIncludeFileCollection(result), primitiveLibrary, allow_open_pins, quitOnError);
             } catch (FileNotFoundException e) {
                 LogFile.err().println(e.toString());
                 System.exit(1);
@@ -135,29 +105,86 @@ public class MergeParserCommandGroup extends JEdifParserCommandGroup {
         return top;
     }
 
-    public static Collection<String> getIncludeFileCollection(JSAPResult result) {
-        return Arrays.asList(result.getStringArray(INCLUDE_FILE_FLAG));
-    }
-
     public static boolean performFlatten(JSAPResult result) {
         return !result.getBoolean(NO_FLATTEN_FLAG);
     }
 
-    
-    public static final String BLACK_BOX_OK_SWITCH = "blackboxes";
-
-    public static final String DIR_FLAG = "dir";
-
-    public static final char DIR_FLAG_SHORT = 'd';
-
-    public static final String FALSE = "false";
-
-    public static final String INCLUDE_FILE_FLAG = "file";
-
-    public static final char INCLUDE_FILE_FLAG_SHORT = 'f';
-
     public static final String NO_FLATTEN_FLAG = "no_flatten";
 
     public static final String NO_OPEN_PINS_SWITCH = "no_open_pins";
+
+}
+
+class BlackBoxSwitch extends Switch {
+
+	// Additional command for processing LUT6_2
+    public BlackBoxSwitch() {
+        super(BLACK_BOX_OK_SWITCH);
+        setShortFlag(JSAP.NO_SHORTFLAG);
+        setLongFlag(BLACK_BOX_OK_SWITCH);
+        setDefault("false");
+        setHelp("Allow parser to continue if blackbox definitions are not found");
+    }
+
+    public static boolean backBoxSwitch(JSAPResult result) {
+    	return result.getBoolean(BLACK_BOX_OK_SWITCH);
+    }
+
+    public static final String BLACK_BOX_OK_SWITCH = "blackboxes";
+}
+
+
+/**
+ * A flagged option for listing the "include" files for an Edif merging operation.
+ */
+class MultipleIncludeFileFlaggedOption extends FlaggedOption {
+
+    public MultipleIncludeFileFlaggedOption() {
+        super(INCLUDE_FILE_FLAG);
+        setStringParser(JSAP.STRING_PARSER);
+        setRequired(JSAP.NOT_REQUIRED);
+        setShortFlag(INCLUDE_FILE_FLAG_SHORT);
+        setLongFlag(INCLUDE_FILE_FLAG);
+        setAllowMultipleDeclarations(JSAP.MULTIPLEDECLARATIONS);
+        setList(JSAP.LIST);
+        setListSeparator(',');
+        setHelp("Explicitly name edif files to include -- including the path to the file.");
+    }
+
+    public static Collection<String> getIncludeFileCollection(JSAPResult result) {
+        return Arrays.asList(result.getStringArray(INCLUDE_FILE_FLAG));
+    }
+
+    public static final String INCLUDE_FILE_FLAG = "file";
+    public static final char INCLUDE_FILE_FLAG_SHORT = 'f';
+
+}
+
+/**
+ * A flagged option for listing the directories to search when parsing and
+ * merging an edif file.
+ */
+class MultipleDirectoryFlaggedOption extends FlaggedOption {
+
+    public MultipleDirectoryFlaggedOption() {
+        super(DIR_FLAG);
+        setStringParser(JSAP.STRING_PARSER);
+        setRequired(JSAP.NOT_REQUIRED);
+        setShortFlag('d');
+        setLongFlag(DIR_FLAG);
+        setAllowMultipleDeclarations(JSAP.MULTIPLEDECLARATIONS);
+        setList(JSAP.LIST);
+        setListSeparator(',');
+        setHelp("Comma-separated list of directories containing external EDIF files to be included.");
+    }
+
+    public static Collection<String> getDirectoryCollection(JSAPResult result) {
+        return Arrays.asList(result.getStringArray(DIR_FLAG));
+    }
+
+    public static final String DIR_FLAG = "dir";
+    public static final char DIR_FLAG_SHORT = 'd';
+
+    public static final String INCLUDE_FILE_FLAG = "file";
 
 }
