ebf6c8c06a48b5d9c8eb530cd893cc511e1874c7
brianpratt <brianpratt@0ac9bcd5-4648-0410-a937-cfa616abf2da>
2010-08-03 20:10:17 +0000

    Increased verbosity of submodule names to ease identification by instance name.
    Added new "diagonal-weighting" for SignedMultiplierGenerator. This tags diagonals with the same weight rather than columns.

--- edu/byu/ece/edif/util/generate/AdderCIN.java
+++ edu/byu/ece/edif/util/generate/AdderCIN.java
@@ -9,11 +9,62 @@ public class AdderCIN extends WeightedModule {
 	}
 	
 	protected void _buildModule() {
+		_buildModule(Integer.MAX_VALUE);
+	}
+	
+	/*
+	protected void _buildModule() {
 		
 		// create full adders
 		FullAdder[] fullAdders = new FullAdder[_width];
 		for (int i = 0; i < _width; i++) {
 			fullAdders[i] = new FullAdder("fa_"+i, _parent, i+_weightOffset);
+		}
+		
+		// wiring
+		addPort("CIN", fullAdders[0].getPort("CIN"));
+		for (int i = 0; i < _width; i++) {
+			connectPort("X", i, fullAdders[i].getPort("X"));
+			connectPort("Y", i, fullAdders[i].getPort("Y"));
+			connectPort("S", i, fullAdders[i].getPort("S"));
+			if (i < (_width-1)) {
+				_parent.connect(fullAdders[i].getPort("COUT"), fullAdders[i+1].getPort("CIN"));
+			}
+		}
+		connectPort("S", _width, fullAdders[_width-1].getPort("COUT"));
+		
+	}
+	*/
+
+	/**
+	 * Includes an alternate way of tagging weights. This is used by the SignedMutliplier
+	 * when tagging weights according to a diagonal-weighted multiplier.
+	 * 
+	 * If not interested in the diagonal multiplier-style adder weights,
+	 * just call with Integer.MAX_VALUE to nullify the effect.
+	 */
+	public AdderCIN(String name, int width, CircuitGenerator generator, int weight, int row) {
+		super(name, generator, weight);
+		_width = width;
+		_buildModule(row);
+	}
+
+	/**
+	 * Includes an alternate way of tagging weights. This is used by the SignedMutliplier
+	 * when tagging weights according to a diagonal-weighted multiplier.
+	 * 
+	 * If not interested in the diagonal multiplier-style adder weights,
+	 * just call with Integer.MAX_VALUE to nullify the effect.
+	 */
+	protected void _buildModule(int row) {
+		
+		// create full adders
+		FullAdder[] fullAdders = new FullAdder[_width];
+		for (int i = 0; i < _width; i++) {
+			// Don't let the module weight exceed it's row value (for diagonal-weighted multipliers)
+			int weight = (row < i) ? row : i;
+			weight += _weightOffset;
+			fullAdders[i] = new FullAdder(_name+"_fa_"+i, _parent, weight);
 		}
 		
 		// wiring
